d726f93aca7fcc40c1f078c184005477dddc539c
AlejandroFerp <alejandrolili9528@gmail.com>
2025-10-05 15:41:01 +0200

    resuelto el 7

---
+++ Solucion-A2.1/A2.1.7_TamanyoTexto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A2._1._7_TamanyoTexto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Solucion-A2.1/A2.1.7_TamanyoTexto/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace A2._1._7_TamanyoTexto
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Selección por defecto
+            foreach (RadioButton rb in new RadioButton[] { (RadioButton)FindName("Pequeño"), (RadioButton)FindName("Mediano"), (RadioButton)FindName("Grande") })
+            {
+                    if (rb != null && rb.Tag != null && rb.Tag.ToString() == "48")
+                {
+                    rb.IsChecked = true;
+                }
+            }
+        }
+
+        private void Tamanyo_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton rb && rb.IsChecked == true)
+            {
+                if (double.TryParse(rb.Tag.ToString(), out double tamaño))
+                {
+                    txtEjemplo.FontSize = tamaño;
+                }
+            }
+        }
+    }
+}
