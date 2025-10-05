1af2817637bde2e8d86912f09dbb28e3ef990458
AlejandroFerp <alejandrolili9528@gmail.com>
2025-10-05 13:54:59 +0200

    primera solucion adivinar numero

---
+++ Solucion-A2.1/1_AdivinaNumero/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+
+namespace _1_AdivinaNumero
+{
+    public partial class MainWindow : Window
+    {
+        private int numeroSecreto;
+        private Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenerarNuevoNumero();
+        }
+
+        private void GenerarNuevoNumero()
+        {
+            numeroSecreto = random.Next(0, 101);
+            lblResultado.Text = "";
+            txtNumero.Text = "";
+        }
+
+        private void btnComprobar_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtNumero.Text, out int numeroUsuario))
+            {
+                if (numeroUsuario < 0 || numeroUsuario > 100)
+                {
+                    lblResultado.Text = "Introduce un número entre 0 y 100.";
+                }
+                else if (numeroUsuario < numeroSecreto)
+                {
+                    lblResultado.Text = "Demasiado bajo";
+                }
+                else if (numeroUsuario > numeroSecreto)
+                {
+                    lblResultado.Text = "Demasiado alto";
+                }
+                else
+                {
+                    lblResultado.Text = "¡Has acertado!";
+                }
+            }
+            else
+            {
+                lblResultado.Text = "Introduce un número válido.";
+            }
+        }
+
+        private void btnReiniciar_Click(object sender, RoutedEventArgs e)
+        {
+            GenerarNuevoNumero();
+        }
+    }
+}
