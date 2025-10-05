23de28a638f3b01c77e4b152bccd003987a681b7
AlejandroFerp <alejandrolili9528@gmail.com>
2025-10-05 15:08:21 +0200

    completado el ejercicio 4

---
+++ Solucion-A2.1/A2.1.4_CaracteresLimitados/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A2._1._4_CaracteresLimitados
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Solucion-A2.1/A2.1.4_CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace A2._1._4_CaracteresLimitados
+{
+    public partial class MainWindow : Window
+    {
+        private const int MaxCaracteres = 140;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtContador.Text = $"0 / {MaxCaracteres}";
+        }
+
+        private void txtContenido_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int longitud = txtContenido.Text.Length;
+
+            // Actualizar contador
+            txtContador.Text = $"{longitud} / {MaxCaracteres}";
+
+            // Bloquear edición si se alcanzó el máximo
+            if (longitud >= MaxCaracteres)
+            {
+                txtContenido.IsReadOnly = true;
+            }
+            else
+            {
+                txtContenido.IsReadOnly = false;
+            }
+        }
+    }
+}
