ff1af161fb276a0da41061a7b2b4f41799885ac9
AlejandroFerp <alejandrolili9528@gmail.com>
2025-10-05 15:19:21 +0200

    completado el 5

---
+++ Solucion-A2.1/A2.1.5_FormatoTexto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A2._1._5_FormatoTexto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Solucion-A2.1/A2.1.5_FormatoTexto/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace A2._1._5_FormatoTexto
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void chkFormato_Changed(object sender, RoutedEventArgs e)
+        {
+            // Aplicar negrita y cursiva
+            txtContenido.FontWeight = (chkNegrita.IsChecked == true) ? FontWeights.Bold : FontWeights.Normal;
+            txtContenido.FontStyle = (chkCursiva.IsChecked == true) ? FontStyles.Italic : FontStyles.Normal;
+        }
+
+        private void rbColor_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton rb)
+            {
+                string color = rb.Tag.ToString();
+                switch (color)
+                {
+                    case "Blue":
+                        txtContenido.Foreground = Brushes.Blue;
+                        break;
+                    case "Red":
+                        txtContenido.Foreground = Brushes.Red;
+                        break;
+                    case "Green":
+                        txtContenido.Foreground = Brushes.Green;
+                        break;
+                }
+            }
+        }
+    }
+}
