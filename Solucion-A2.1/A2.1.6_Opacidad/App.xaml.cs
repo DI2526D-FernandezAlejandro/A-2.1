5b5df416680fb25200f5f2a398331a51be7ac707
AlejandroFerp <alejandrolili9528@gmail.com>
2025-10-05 15:25:17 +0200

    resuelto el 6

---
+++ Solucion-A2.1/A2.1.6_Opacidad/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A2._1._6_Opacidad
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Solucion-A2.1/A2.1.6_Opacidad/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+
+namespace A2._1._6_Opacidad
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Evento único para todas las imágenes
+        private void Imagen_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Image img)
+            {
+                img.Opacity = 1.0; // Opacidad 100%
+            }
+        }
+
+        private void Imagen_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Image img)
+            {
+                img.Opacity = 0.5; // Opacidad 50%
+            }
+        }
+    }
+}
